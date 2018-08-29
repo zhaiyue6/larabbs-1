7cf08bfb345b858801eafad227a4cb1d1ad45f51
zhaiyue6 <1135019812@qq.com>
2018-08-29 11:25:49 +0800

    Initial commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,13 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
